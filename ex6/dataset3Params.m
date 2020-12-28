eaf13e8774fd5430db0dfea8f40b1a22516b05e9
Weslei Rodrigues <69604469+crazywes@users.noreply.github.com>
2020-12-28 15:51:05 -0300

    Add files via upload

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,12 +22,33 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+c_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+row = 0;
+for i = 1:size(c_list, 2) %intera nas listas encontrando o melhor valor de c e sigma
+    c_temp = c_list(i);
 
+    for j = 1:size(sigma_list, 2)
+        row += 1;
+        sigma_temp = sigma_list(j);
+        model = svmTrain(X, y, c_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp));
 
+        pred = svmPredict(model, Xval);
+        dif = mean(double(pred ~= yval));
 
+        %add valores na lista melhor
+        valores(row, :) = [dif, c_temp, sigma_temp];
+    endfor
+endfor
 
+%% Encontra o melhor valor
 
+menor = min(valores(:, 1));
+ind = find(menor==valores(:,1));
+
+C = valores(ind,2);
+sigma = valores(ind,3);
 
 % =========================================================================
 
